319d0e230cc1c9339ff19a3b025d26419e87f41b
Jim <jimke2000@gmail.com>
2021-03-16 20:05:02 +1100

    auth changes

--- auth.js
+++ auth.js
@@ -1,6 +1,5 @@
 const db = require("quick.db");
 const score = require("./script.js");
-var logonFailedAlert = document.getElementById("alert");
 
 $(document).ready(function () {
   //$("#timerBar").hide(); would hide an element with the ID timerBar
